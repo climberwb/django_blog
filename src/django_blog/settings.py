70cfaa9fda4a0f96b6ad3cb25c5f95bbdeedfdc7
Warren Kushner <warren.kushner@gmail.com>
2016-04-20 13:58:55 +0000

    added comments application
    * added post foreign key to comments

--- src/django_blog/settings.py
+++ src/django_blog/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    ## local apps
+    'comments',
     'posts',
+    
 ]
 
 MIDDLEWARE_CLASSES = [
